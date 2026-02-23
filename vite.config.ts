b612af9b197bb88467f6f7c38ba450ed66194f30
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2026-02-23 21:02:39 +0300

    feat: css-module vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { patchCssModules } from 'vite-css-modules'
 
 export default defineConfig({
-	plugins: [react()]
+	plugins: [
+		react(),
+		patchCssModules({
+			generateSourceTypes: true,
+			declarationMap: true,
+		}),
+	],
 })
