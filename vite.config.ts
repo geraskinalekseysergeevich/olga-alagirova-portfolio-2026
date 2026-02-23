9e77bc7c05f2ff537087daa37917bef54f760e37
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2026-02-23 22:08:11 +0300

    fix: remove css-module vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { patchCssModules } from 'vite-css-modules'
 
 export default defineConfig({
-	plugins: [
-		react(),
-		patchCssModules({
-			generateSourceTypes: true,
-			declarationMap: true,
-		}),
-	],
+	plugins: [react()],
 })
