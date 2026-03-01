9bef77c759e308ff55b950be10b6be4dff68ea13
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2026-03-01 18:31:42 +0300

    fix: scroll position on new page

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { useLayoutEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 
 import { Navbar } from '../navbar/navbar'
 
 export const Layout = () => {
+	const { pathname, hash } = useLocation()
+
+	useLayoutEffect(() => {
+		if (hash) {
+			return
+		}
+
+		window.scrollTo(0, 0)
+	}, [pathname, hash])
+
 	return (
 		<>
 			<Navbar />
