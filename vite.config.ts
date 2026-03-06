5d9990780a857f2a62e54e3cf27e6f463e1cf681
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2026-03-06 03:01:34 +0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	base: '/olga-portfolio-2026/',
+	base: '/olga-alagirova-portfolio-2026/',
 	plugins: [react()],
 })
