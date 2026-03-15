ef62170adae72f027bdfdff9b3b8021fe11801d6
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2026-03-15 14:35:06 +0300

    fix: router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter, Navigate } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 
 import { Layout } from '../components/layout/layout'
 import { AplasticPage } from '../pages/cases/aplastic/aplastic'
@@ -23,63 +23,68 @@ import {
 	VK_LIGHTS_ROUTE,
 } from './routes'
 
-export const router = createHashRouter([
-	{
-		element: <Layout />,
-		children: [
-			{
-				path: MAIN_ROUTE,
-				element: <MainPage />,
-			},
-			{
-				path: CV_ROUTE,
-				element: <CvPage />,
-			},
-			{
-				path: CASES_ROUTE,
-				children: [
-					{
-						index: true,
-						element: <Navigate replace to={MAIN_ROUTE} />,
-					},
-					{
-						path: APLASTIC_ROUTE,
-						element: <AplasticPage />,
-					},
-					{
-						path: SBER_SELLER_ROUTE,
-						element: <SberSellerPage />,
-					},
-					{
-						path: GKB_DAVYDOVSKY_ROUTE,
-						element: <GkbDavydovskyPage />,
-					},
-					{
-						path: VK_LIGHTS_ROUTE,
-						element: <VkLightsPage />,
-					},
-					{
-						path: HAHNANAGAS_ROUTE,
-						element: <HahnanagasPage />,
-					},
-					{
-						path: EGGLESTON_ROUTE,
-						element: <EgglestonPage />,
-					},
-					{
-						path: SOFIA_SONECHKA_ROUTE,
-						element: <SofiaSonechkaPage />,
-					},
-					{
-						path: '*',
-						element: <Navigate replace to={MAIN_ROUTE} />,
-					},
-				],
-			},
-			{
-				path: '*',
-				element: <Navigate replace to={MAIN_ROUTE} />,
-			},
-		],
-	},
-])
+const basename = typeof window === 'undefined' ? undefined : window.__APP_BASENAME__
+
+export const router = createBrowserRouter(
+	[
+		{
+			element: <Layout />,
+			children: [
+				{
+					path: MAIN_ROUTE,
+					element: <MainPage />,
+				},
+				{
+					path: CV_ROUTE,
+					element: <CvPage />,
+				},
+				{
+					path: CASES_ROUTE,
+					children: [
+						{
+							index: true,
+							element: <Navigate replace to={MAIN_ROUTE} />,
+						},
+						{
+							path: APLASTIC_ROUTE,
+							element: <AplasticPage />,
+						},
+						{
+							path: SBER_SELLER_ROUTE,
+							element: <SberSellerPage />,
+						},
+						{
+							path: GKB_DAVYDOVSKY_ROUTE,
+							element: <GkbDavydovskyPage />,
+						},
+						{
+							path: VK_LIGHTS_ROUTE,
+							element: <VkLightsPage />,
+						},
+						{
+							path: HAHNANAGAS_ROUTE,
+							element: <HahnanagasPage />,
+						},
+						{
+							path: EGGLESTON_ROUTE,
+							element: <EgglestonPage />,
+						},
+						{
+							path: SOFIA_SONECHKA_ROUTE,
+							element: <SofiaSonechkaPage />,
+						},
+						{
+							path: '*',
+							element: <Navigate replace to={MAIN_ROUTE} />,
+						},
+					],
+				},
+				{
+					path: '*',
+					element: <Navigate replace to={MAIN_ROUTE} />,
+				},
+			],
+		},
+	],
+	{ basename },
+)
